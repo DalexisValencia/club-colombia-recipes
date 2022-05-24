6a07f6828a7f39f924e72a904263aaf43189f845
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-24 16:55:12 -0500

    Empenzado página de detalle de las recetas

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
     $(document).ready(function () {
         const mainBanner = $(".main-recipes__banner-container.owl-carousel")
-        //jquery base
+        //all recipes banner carousel 
         if(mainBanner) {
             mainBanner.owlCarousel({
                 loop: true,
@@ -29,6 +29,21 @@
                         dotsContainer: $(".main-recipes__banner-dots"),
                     }
                 }
+            });
+        }
+        //Detail
+
+        //Play button
+        const playBtn = $(".ingredients-video__play");
+        
+        if(playBtn){
+            playBtn.on("click", function(){
+                var image = $(".ingredients-video__cover");
+                var iframe = $(".ingredients-video__iframe");
+
+                $(this).addClass("playing");
+                image.addClass("playing");
+                iframe.attr("src", iframe.attr("src")+"?autoplay=1").addClass("playing");
             });
         }
     });
